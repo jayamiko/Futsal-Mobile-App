b8453436c0c3b380b178e4f96e5ef211d9b06495
jayamiko <jayamiko4@gmail.com>
2021-12-21 14:24:44 +0700

    add tab navigator

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text
+                style={{ flex: 1, alignItems: 'center', color: 'black' }}
+            >Home Screen</Text>
+        </View>
+    )
+}
+
+export default Home;
+
+const styles = StyleSheet.create({});
